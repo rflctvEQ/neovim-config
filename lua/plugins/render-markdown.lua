9d14ca2373edcfb979d5450c6d3856218e09cce6
rflctvEQ <ian.andrew.everitt@gmail.com>
2025-09-19 23:55:10 -0500

    render-markdown plugin added

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,13 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons", -- or mini.nvim / mini.icons
+  },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    -- your config goes here
+  },
+}
+
